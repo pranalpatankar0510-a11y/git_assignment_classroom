45a58d70f0ecab2ce9cf62173b1082bcc03ff7a5
Pranal <pranal.patankar.0510@gmail.com>
2025-09-07 01:18:34 +0530

    stash done and completed the remaining changes

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 console.log("payment setting added");
 console.log("payment setting added for lab3 in main and payment_gateway branch");
 console.log("conflict resolved manually");
+console.log("added for lab 6 stash command");
